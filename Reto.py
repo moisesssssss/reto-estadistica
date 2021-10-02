ff31ba6b5f27918ea0cfc5e2dbff04068657e1cc
Skuviduvi <83040898+Skuviduvi@users.noreply.github.com>
2021-10-02 18:02:54 -0500

    Create Reto.py

---
+++ Reto.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Importar datos de pydataset
+#Checar bases de datos
+#data()
+#df = data('iris')
+#Cargar base de datos
+df = sns.load_dataset('iris')
+
